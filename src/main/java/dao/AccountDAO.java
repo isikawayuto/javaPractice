537a31ede117bd97cc0af1f8141c9da82d3d911f
イシカワユウト <113403847+isikawayuto@users.noreply.github.com>
2023-01-24 10:11:00 +0900

    Update AccountDAO.java

--- src/main/java/dao/AccountDAO.java
+++ src/main/java/dao/AccountDAO.java
@@ -124,4 +124,40 @@ public class AccountDAO {
 		}
 		return null;
 	}
+	public static List<Account> selectAllHuman() {
+		// 返却用変数
+		List<Account> result = new ArrayList<>();
+
+		String sql = "SELECT * FROM gitpractice";
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+		
+
+			try (ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					int id = rs.getInt("id");
+					String name = rs.getString("name");
+					String age = rs.getString("age");
+					String gender = rs.getString("gender");
+					String birthday = rs.getString("birthday");
+					String address = rs.getString("address");
+					String tel = rs.getString("tel");
+					String mail = rs.getString("mail");
+
+					Account account = new Human(id, name, age, gender, birthday, address, tel, mail null, null);
+					
+					result.add(account);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		// Listを返却する。0件の場合は空のListが返却される。
+		return result;
+	}
 }
